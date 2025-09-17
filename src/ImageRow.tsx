6ca5528dccf0f945ce302c7608e4276d44e54698
praiseee <S10255495@connect.np.edu.sg>
2025-09-17 18:55:12 +0700

    Mask Effect on logos

--- src/ImageRow.tsx
+++ src/ImageRow.tsx
@@ -7,11 +7,14 @@ interface ImagesRowProps {
 
 export default function ImageRow({ images }: ImagesRowProps) {
   return (
-    <div className="flex gap-20 justify-center py-6">
+
+    // overflow-hidden — so images outside the container are hidden. mask creates the fade on the left and right edges.
+    <div className="flex gap-20 justify-center py-6 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_550px,_black_calc(100%-550px),transparent_100%)]">
       {images.map((src, idx) => (
         <img key={idx} src={src} alt={`Image ${idx + 1}`} 
         className="max-w-16 max-h-16 object-contain "/> //unique key for React to track elements
       ))}
     </div>
+
   );
 }
