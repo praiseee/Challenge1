d41a2fb7fa9e11b7e0c05c595ff6a3c8564c69ec
praiseee <S10255495@connect.np.edu.sg>
2025-09-18 12:46:38 +0700

    Image Row Animation Added

--- src/ImageRow.tsx
+++ src/ImageRow.tsx
@@ -9,30 +9,26 @@ export default function ImageRow({ images }: ImagesRowProps) {
   return (
     <div className="flex w-full overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_550px,_black_calc(100%-550px),transparent_100%)]">
       
-      {/* First row */}
+      {/* First row Animation*/}
       <div className="flex gap-20 animate-infinite-scroll"> 
         {images.map((src, idx) => (
           <img
             key={idx}
             src={src}
-            alt={`Image ${idx + 1}`}
-            className="max-w-16 max-h-16 object-contain"
-          />
+            alt={`Image ${idx}`}
+            className="max-w-16 max-h-16 object-contain"/>
         ))}
       </div>
 
       {/* Duplicate row for looping */}
       <div
-        className="flex gap-20 animate-infinite-scroll"
-        aria-hidden="true"
-      >
+        className="flex gap-20 animate-infinite-scroll">
         {images.map((src, idx) => (
           <img
             key={`dup-${idx}`}
             src={src}
-            alt=""
-            className="max-w-16 max-h-16 object-contain"
-          />
+            alt={`Image ${idx}`}
+            className="max-w-16 max-h-16 object-contain"/>
         ))}
       </div>
     </div>
