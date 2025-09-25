dad9e4ab124d683207abafc4c8e6d096c83153d6
praiseee <S10255495@connect.np.edu.sg>
2025-09-25 16:14:15 +0700

    Update Hover Btn

--- src/components/hoverBtn.tsx
+++ src/components/hoverBtn.tsx
@@ -1,21 +1,21 @@
-
 const HoverBtn = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-
       <button className="relative px-6 py-3 font-bold text-white bg-blue-600 rounded overflow-hidden group h-12 w-32">
+        
         {/* Default text */}
-        {/*Use <span> when you want just a container to style*/}
-        <span className="absolute inset-0 flex items-center justify-center transform transition-transform duration-500 group-hover:-translate-y-full">
-          Click Me
+        <span className="absolute inset-0 flex items-center justify-center transform-gpu [transform:translate3d(0,0%,0)]
+        transition-all duration-500 group-hover:[transform:translate3d(0,-100%,0)] group-hover:opacity-0">
+          Up
         </span>
 
         {/* Hover text */}
-        <span className="absolute inset-0 flex items-center justify-center transform translate-y-full transition-transform duration-500 group-hover:translate-y-0">
-          Let's Go!
+        <span className="absolute inset-0 flex items-center justify-center transform-gpu [transform:translate3d(0,100%,0)]
+        transition-all duration-500 group-hover:[transform:translate3d(0,0%,0)] opacity-0 group-hover:opacity-100">
+          Down
         </span>
+        
       </button>
-      
     </div>
   );
 };
