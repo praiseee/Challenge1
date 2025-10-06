ff1dc7c57453dae558cd38389953b92a462a893a
praiseee <S10255495@connect.np.edu.sg>
2025-10-07 02:38:26 +0700

    Updated Circle Toggle - Strike

--- src/components/TodoComp/TodoItem.tsx
+++ src/components/TodoComp/TodoItem.tsx
@@ -24,34 +24,33 @@ const TodoItem: React.FC<TodoItemProps> = ({
   toggleDone,
   handleDelete,
 }) => {
+
   return (
     <div className="flex items-center gap-3">
-      {/* Done Circle */}
-      <div
-        onClick={() => toggleDone(id)}
-        className={`w-5 h-5 rounded-full border border-gray-500 flex items-center justify-center cursor-pointer transition-colors shrink-0 ${
-          done ? "bg-blue-500 border-blue-500" : "bg-transparent"
-        }`}
-      ></div>
+        {/* Done Circle */}
+        <div
+            onClick={() => toggleDone(id)}
+            className={`w-5 h-5 rounded-full border border-gray-500 flex items-center justify-center cursor-pointer transition-colors shrink-0 ${
+            done ? "bg-blue-500 border-blue-500" : "bg-transparent"}`}>
+        </div>
 
-      {/* Text Input */}
-      <input
+        {/* Text Input */}
+        <input
         ref={isLast ? lastInputRef : null} // focus on last input
         type="text"
         value={value}
         onChange={(e) => handleChange(id, e.target.value)}
         onKeyDown={(e) => handleKeyDown(e, id, value)}
         placeholder="Type a task..."
-        className="text-sm text-gray-500 border-b border-gray-300 focus:outline-none flex-1 py-1"
-      />
+        className={`text-sm border-b border-gray-300 focus:outline-none flex-1 py-1 ${done? "line-through text-gray-400":"text-gray-500"}`}/>
 
-      {/* Delete Button */}
-      <div
-        onClick={() => handleDelete(id)}
-        className="text-sm px-3 py-1 rounded-3xl flex items-center justify-center border border-gray-300 flex-shrink-0 cursor-pointer hover:bg-gray-100 transition"
-      >
-        Delete
-      </div>
+        {/* Delete Button */}
+        <div
+            onClick={() => handleDelete(id)}
+            className="text-sm px-3 py-1 rounded-3xl flex items-center justify-center border border-gray-300 flex-shrink-0 cursor-pointer hover:bg-gray-100 transition">
+            Delete
+        </div>
+
     </div>
   );
 };
